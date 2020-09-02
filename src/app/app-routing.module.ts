b9949628c816fb407ee8bd115e591d58c324368e
sirlocust <jua56yu@hotmail.com>
2020-09-02 11:28:13 -0500

    creaando rutas hijas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { LayoutComponent } from './layout/layout.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ContactsComponent } from './contacts/contacts.component';
@@ -9,22 +10,26 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path:'',
-    redirectTo: '/home',
-    pathMatch:'full'
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+      { path: 'products', component: ProductsComponent },
+      { path: 'contact', component: ContactsComponent },
+      {
+        path: 'products/:id',
+        component: ProductDetailComponent,
+      },
+    ]
   },
-  {
-    path: 'home',
-    component: HomeComponent,
-  },
-  { path: 'products', component: ProductsComponent },
-  { path: 'contact', component: ContactsComponent },
-  {
-    path: 'products/:id',
-    component:ProductDetailComponent
-  },
-  {path: '**',
-    component: PageNotFoundComponent  
-}
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
